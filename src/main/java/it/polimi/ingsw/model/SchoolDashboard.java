f80d3ea894bde50311fc86c6a0a7d05148024bdb
FedericoMansutti <federico.mansutti@mail.polimi.it>
2022-04-26 12:47:44 +0200

    Updated School Dashboard

--- src/main/java/it/polimi/ingsw/model/SchoolDashboard.java
+++ src/main/java/it/polimi/ingsw/model/SchoolDashboard.java
@@ -1,9 +1,13 @@
 package it.polimi.ingsw.model;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 
 public class SchoolDashboard {
     private ArrayList<Professor> professors;
     private int towers;
+    private EnumMap<Color,Integer> lounge;
+    private EnumMap<Color,Integer> lobby;
+
 
 }
